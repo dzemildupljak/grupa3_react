9e7a673f62080156688d32ddcd0acb4aab91a8d2
dzemildupljak <dzemildupljak4795@gmail.com>
2021-10-23 19:35:54 +0200

    pagination finished

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -5,16 +5,23 @@ import CardList from "./components/cat_facts/cardList/CardList";
 import PageButtons from "./components/page_buttons/PageButtons";
 
 function App() {
-  const [factsArr, setFactsArr] = useState([]);
+  const [factsArr, setFactsArr] = useState({ data: [] });
   const [limit, setLimit] = useState(5);
   const [page, setPage] = useState(1);
 
   const getData = async () => {
-    let response = await axios.get(
-      `https://catfact.ninja/facts?limit=${limit}&page=${page}`
-    );
-    console.log(response.data);
-    setFactsArr(response.data.data);
+    try {
+      var response = await axios.get(
+        `https://centarnitbe.herokuapp.com/user/all`
+      );
+      // var response = await axios.get(
+      //   `https://catfact.ninja/facts?limit=${limit}&page=${page}`
+      // );
+      console.log(response.data);
+      setFactsArr(response.data);
+    } catch (error) {
+      console.log("this is my error:=====", error);
+    }
   };
   useEffect(() => {
     getData();
@@ -30,10 +37,12 @@ function App() {
     }
   };
   const nextPageHandler = () => {
-    if (page < 332) {
+    if (page < 67) {
       setPage(page + 1);
     }
   };
+
+  const handleSpecificPage = (page_num) => setPage(page_num);
 
   return (
     <div>
@@ -51,19 +60,71 @@ function App() {
         <option value={50}>50</option>
       </select>
       <PageButtons handlerFunc={getData} text="Fetch data" type="danger" />
-      <CardList listFacts={factsArr} />
+      <CardList listFacts={factsArr.data} />
       <div className="pagination">
         <PageButtons
           handlerFunc={prevPageHandler}
           text="< Prev page"
           type="warning"
         />
-        <h3>{page}</h3>
+        <h3
+          onClick={() => {
+            handleSpecificPage(factsArr.from);
+          }}
+        >
+          {factsArr.from}
+        </h3>
+        <h3
+          onClick={() => {
+            handleSpecificPage(factsArr.from + 1);
+          }}
+        >
+          {factsArr.from + 1}
+        </h3>
+        <h3>...</h3>
+        {page > 1 ? (
+          <h3
+            onClick={() => {
+              handleSpecificPage(factsArr.current_page - 1);
+            }}
+          >
+            {page - 1}
+          </h3>
+        ) : (
+          <h3></h3>
+        )}
+        <h3>{factsArr.current_page}</h3>
+        {page < 66 ? (
+          <h3
+            onClick={() => {
+              handleSpecificPage(factsArr.current_page + 1);
+            }}
+          >
+            {page + 1}
+          </h3>
+        ) : (
+          <h3></h3>
+        )}
+        <h3>...</h3>
+        <h3
+          onClick={() => {
+            handleSpecificPage(factsArr.last_page - 1);
+          }}
+        >
+          {factsArr.last_page - 1}
+        </h3>
+        <h3
+          onClick={() => {
+            handleSpecificPage(factsArr.last_page);
+          }}
+        >
+          {factsArr.last_page}
+        </h3>
         <PageButtons
           handlerFunc={nextPageHandler}
           text="Next page >"
           type="default"
-          />
+        />
       </div>
     </div>
   );
